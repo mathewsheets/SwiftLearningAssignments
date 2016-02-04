9c61257f944a9e4a93154cc2a2debc2cfe54c960
Mathew Sheets <mathew.sheets@gmail.com>
2016-02-03 21:09:50 -0500

    iterating over tuples

--- Todo_Assignment_1.playground/Contents.swift
+++ Todo_Assignment_1.playground/Contents.swift
@@ -1,14 +1,32 @@
 
 import Foundation
 
-var todo1 = (title: "First Todo", date: "01-31-16", desc: "My first todo item", assigned: (first: "Mathew", last: "Sheets"))
-var todo2 = (title: "Second Todo", date: "01-31-16", desc: "My second todo item", assigned: (first: "Jack", last: "Sheets"))
+var todos = (
+    (id: 1, title: "First Todo", date: "01-31-16", desc: "My first todo item", assigned: (first: "Mathew", last: "Sheets")),
+    (id: 2, title: "Second Todo", date: "01-31-16", desc: "My second todo item", assigned: (first: "Jack", last: "Sheets"))
+)
 
-var todos = (todo1, todo2)
+//iterate(todos) { (label, value) -> (label: String, value: Any) in
+//    
+//    print("\(label) - \(value)")
+//    
+//    return (label, value)
+//}
 
-iterate(todos) { (label, value) -> (label: String, value: Any) in
-    
-    print("\(label) - \(value)")
-    
-    return (label, value)
+let todosMirror = Mirror(reflecting: todos)
+for case let (label?, value) in todosMirror.children {
+
+    print(label)
+
+    let todoMirror = Mirror(reflecting: value)
+    for case let (label?, value) in todoMirror.children {
+        
+        print("\t\(value)")
+        
+        let nameMirror = Mirror(reflecting: value)
+        for case let (label?, value) in nameMirror.children {
+            
+            print("\t\t\(value)")
+        }
+    }
 }
