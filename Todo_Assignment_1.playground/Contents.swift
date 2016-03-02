cdfe25537162ee8d40f3a4748bb2da9b5c57b654
Mathew Sheets <mathew.sheets@gmail.com>
2016-03-01 21:41:52 -0500

    Added markup for each assignment

--- Todo_Assignment_1.playground/Contents.swift
+++ Todo_Assignment_1.playground/Contents.swift
@@ -1,3 +1,16 @@
+/*:
+**Assignment:** Based on Sessions 1 - 3 (Basics | DataTypes | Operators, Strings & Characters, Control Flow), create a playground that will manage your todos.
+>> **You will need to:**
+* Print all your Todos (small description)
+* Print a single Todo (large description)
+* Add a Todo
+* Update a Todo
+* Delete a Todo
+>> **Constraints:**
+* Use a tuple to model each Todo
+* Use a single tuple to group all your Todos
+* Investigate what a `Mirror` is and how it can be leveraged
+*/
 import Foundation
 
 let todos = (
