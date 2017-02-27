107aa12a4e3a42111110b357a0a58cde53257466
Mathew Sheets <mathew.sheets@gmail.com>
2017-02-27 08:11:42 -0500

    Assignment Solutions

--- Todo_Assignment_2.playground/Sources/Data.swift
+++ Todo_Assignment_2.playground/Sources/Data.swift
@@ -4,7 +4,7 @@ public var todos = [Todo]()
 
 public func initData() {
     
-    addTodo(todo: createTodo(title: "Weed", subtitle: "Flower beds and garden", description: "Need to weed the flower beds and garden. Get one of the buys to help."))
-    addTodo(todo: createTodo(title: "Study", subtitle: "Swift Exam coming up", description: "Study for the swift exam coming up in the next few weeks"))
-    addTodo(todo: createTodo(title: "Food", subtitle: "Saturday date night dinner", description: "Need to get food for Saturday's date night"))
+    _ = addTodo(todo: createTodo(title: "Weed", subtitle: "Flower beds and garden", description: "Need to weed the flower beds and garden. Get one of the buys to help."))
+    _ = addTodo(todo: createTodo(title: "Study", subtitle: "Swift Exam coming up", description: "Study for the swift exam coming up in the next few weeks"))
+    _ = addTodo(todo: createTodo(title: "Food", subtitle: "Saturday date night dinner", description: "Need to get food for Saturday's date night"))
 }
